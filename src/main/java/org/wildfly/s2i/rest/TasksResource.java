2cf19ab8d199306b86f81a231f7df7ea1e44ada9
Yeray Borges <yborgess@redhat.com>
2020-04-01 11:58:13 +0100

    Remove unneeded transaction

--- src/main/java/org/wildfly/s2i/rest/TasksResource.java
+++ src/main/java/org/wildfly/s2i/rest/TasksResource.java
@@ -1,12 +1,9 @@
 package org.wildfly.s2i.rest;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.UserTransaction;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -21,19 +18,8 @@ public class TasksResource {
     @PersistenceContext
     private EntityManager em;
 
-    @Inject
-    private UserTransaction tx;
-
     @GET
     public List<Task> getAllTasks() {
-        List<Task> tasks = new ArrayList<>();
-        try {
-            tx.begin();
-            tasks = em.createNamedQuery(Task.FIND_ALL).getResultList();
-            tx.commit();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        return tasks;
+        return em.createNamedQuery(Task.FIND_ALL).getResultList();
     }
 }
